5686889b00a330bb0117f078421dab76daa5afd4
hubert-kniola <hubert.kniola@gmail.com>
2020-11-11 02:57:08 +0100

    changes

--- Calculations/Calculate.cs
+++ Calculations/Calculate.cs
@@ -3,63 +3,13 @@ using System.Collections.Generic;
 using System.Numerics;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 
 namespace Calculations
 {
     public class Calculate
     {
-        public static TimeSpan Time(Action action)
-        {
-            Stopwatch stopwatch = Stopwatch.StartNew();
-            action();
-            stopwatch.Stop();
-            return stopwatch.Elapsed;
-        }
-        public static void difiAlgorithm()
-        {
-            Stopwatch sw = Stopwatch.StartNew();
-            BigInteger n, g;
-            TimeSpan timeN, timeG;
-            List<BigInteger> listG;
-
-            sw.Start();
-            n = GeneratePrime();
-            sw.Stop();
-            timeN = sw.Elapsed;
-            sw.Start();
-            g = FindPrimitive(n);
-            sw.Stop();
-            timeG = sw.Elapsed;
-
-            Console.WriteLine($"Primitive root: {IsPrimitiveRoot(g, n)}");
-            Console.WriteLine($"Value of n: {n} | Generation time [ms]: {timeN}");
-            Console.WriteLine($"Value of g: {g} | Generation time [ms]: {timeG}");
-            BigInteger x = 0;
-            BigInteger y = 0;
-            sw.Start();
-            var ix = GenerateXY(10, 100); //male x
-            sw.Stop();
-            Console.WriteLine($"Value of x: {ix} | Generation time [ms]: {sw.Elapsed}");
-            sw.Start();
-            var iy = GenerateXY(10, 100); //male y
-            sw.Stop();
-            Console.WriteLine($"Value of y: {iy} | Generation time [ms]: {sw.Elapsed}");
-
-            sw.Start();
-            x = GenKey(g, ix, n); //duze X
-            sw.Stop();
-            Console.WriteLine($"Private Key: {x}, generating [ms]: {sw.Elapsed}");
-            sw.Start();
-            y = GenKey(g, iy, n); //duze Y
-            sw.Stop();
-            Console.WriteLine($"Public Key: {y}, generating [ms]: {sw.Elapsed}");
-
-
-            var kA = GenKey(y, ix, n);
-            var kB = GenKey(x, iy, n);
-            Console.WriteLine($"kA: {kA}\nkB: {kB}");
-        }
-
+        #region Methodes
         private static BigInteger Power(BigInteger x, BigInteger y, BigInteger p)
         {
             BigInteger res = 1;
@@ -97,6 +47,11 @@ namespace Calculations
                 s.Add(n);
         }
 
+        /// <summary>
+        /// Metoda szukająca pierwiastka pierwotnego
+        /// </summary>
+        /// <param name="n">Liczba pierwsza</param>
+        /// <returns></returns>
         private static BigInteger FindPrimitive(BigInteger n)
         {
             List<BigInteger> s = new List<BigInteger>();
@@ -112,7 +67,7 @@ namespace Calculations
                 bool flag = false;
                 foreach (int a in s)
                 {
-                    if (Power(r, phi/a, n) == 1)
+                    if (Power(r, phi / a, n) == 1)
                     {
                         flag = true;
                         break;
@@ -124,36 +79,26 @@ namespace Calculations
             return -1;
         }
 
-        private static BigInteger GenerateNumber(BigInteger N)
-        {
-            Random rnx = new Random();
-            BigInteger sd = 0;
-            do
-                sd = rnx.Next(100, 10000000);
-            while (BigInteger.GreatestCommonDivisor(sd, N) != 1);
-            return sd;
-        }
-
-        private static List<BigInteger> PrimitiveRoots(BigInteger modulo)
+        private static List<BigInteger> PrimitiveRoots(BigInteger g)
         {
             var coprimes = new List<BigInteger>();
 
-            for (int i = 1; i < modulo; i++)
+            for (int i = 1; i < g; i++)
             {
-                if (BigInteger.GreatestCommonDivisor(i, modulo) == 1)
+                if (BigInteger.GreatestCommonDivisor(i, g) == 1)
                 {
                     coprimes.Add(i);
                 }
             }
 
             var primitiveRoots = new List<BigInteger>();
-            for (int j = 1; j < modulo; j++)
+            for (int j = 1; j < g; j++)
             {
                 var resultSet = new List<BigInteger>();
 
-                for (int k = 1; k < modulo; k++)
+                for (int k = 1; k < g; k++)
                 {
-                    resultSet.Add(BigInteger.Pow(j, k) % modulo);
+                    resultSet.Add(BigInteger.Pow(j, k) % g);
                 }
 
                 if (coprimes.TrueForAll(resultSet.Contains))
@@ -165,13 +110,19 @@ namespace Calculations
             return primitiveRoots;
         }
 
-        private static bool IsPrimitiveRoot(BigInteger modulo, BigInteger arg)
+        /// <summary>
+        /// Metoda sprawdzająca czy podana liczba jest pierwiastkiem pierwotnym
+        /// </summary>
+        /// <param name="g">Pierwiastek pierwotny</param>
+        /// <param name="n">Liczba pierwsza</param>
+        /// <returns></returns>
+        private static bool IsPrimitiveRoot(BigInteger g, BigInteger n)
         {
             var coprimes = new List<BigInteger>();
 
-            for (int i = 0; i < modulo; i++)
+            for (int i = 0; i < g; i++)
             {
-                if (BigInteger.GreatestCommonDivisor(i, modulo) == 1)
+                if (BigInteger.GreatestCommonDivisor(i, g) == 1)
                 {
                     coprimes.Add(i);
                 }
@@ -179,23 +130,20 @@ namespace Calculations
 
             var resultSet = new List<BigInteger>();
 
-            for (int i = 0; i < modulo; i++)
+            for (int i = 0; i < g; i++)
             {
-                resultSet.Add(BigInteger.Pow(arg, i) % modulo);
+                resultSet.Add(BigInteger.Pow(n, i) % g);
             }
 
             return coprimes.TrueForAll(resultSet.Contains);
         }
 
-        private static BigInteger GeneratePrime()
-        {
-            BigInteger x;
-            do
-                x = GenerateNumber(1000);
-            while (!CheckPrime(x));
-            return x;
-        }
 
+        /// <summary>
+        /// Metoda sprawdzająca czy liczba jest pierwsza
+        /// </summary>
+        /// <param name="n">Liczba pierwsza</param>
+        /// <returns></returns>
         private static bool CheckPrime(BigInteger n)
         {
             var isPrime = true;
@@ -206,21 +154,101 @@ namespace Calculations
                 if ((n % i) == 0) isPrime = false;
             return isPrime;
         }
+#endregion
 
+        #region NumberGenerators
+        /// <summary>
+        /// Metoda sprawdzająca czy wylosowana liczba jest liczbą pierwszą
+        /// </summary>
+        /// <returns></returns>
+        private static BigInteger GeneratePrime()
+        {
+            BigInteger x;
+            do
+                x = GenerateXY(100, 10000000);
+            while (!CheckPrime(x));
+            return x;
+        }
+
+        /// <summary>
+        /// Metoda generująca wartości z podanego zakresu
+        /// </summary>
+        /// <param name="min">Wartość minimalna</param>
+        /// <param name="max">Wartośc maksymalna</param>
+        /// <returns></returns>
         private static BigInteger GenerateXY(int min, int max)
         {
-            var rnx = new Random();
-            return rnx.Next(min, max);
+            Thread.Sleep(100);
+            return new Random().Next(min, max);
         }
 
+        /// <summary>
+        /// Metoda wyliczająca wartość z odpowiedniego wzoru
+        /// </summary>
+        /// <param name="g">Pierwiastek pierwotny</param>
+        /// <param name="ix">Wartość małego x</param>
+        /// <param name="n">Liczba pierwsza</param>
+        /// <returns></returns>
         private static BigInteger GenKey(BigInteger g, BigInteger ix, BigInteger n)
         {
-            return BigInteger.Pow(g, (int)ix) % n;
+            return (BigInteger.Pow(g, (int)ix) % n);
         }
+        #endregion
 
-        private static BigInteger GenK(BigInteger basis, BigInteger pow, BigInteger n)
+        #region MainFunction
+        /// <summary>
+        /// Funkcja główna programu
+        /// </summary>
+        public static void difiAlgorithm()
         {
-            return BigInteger.Pow(basis, (int)pow) % n;
+            Stopwatch sw = Stopwatch.StartNew();
+            BigInteger n, g;
+            TimeSpan timeN, timeG;
+            List<BigInteger> listG;
+
+            #region NG_Generate
+            sw.Start();
+            n = GeneratePrime();
+            sw.Stop();
+            timeN = sw.Elapsed;
+            sw.Start();
+            g = FindPrimitive(n);
+            sw.Stop();
+            timeG = sw.Elapsed;
+
+            Console.WriteLine($"Primitive root: {IsPrimitiveRoot(g, n)}");
+            Console.WriteLine($"Value of n: {n} | Generation time [ms]: {timeN}");
+            Console.WriteLine($"Value of g: {g} | Generation time [ms]: {timeG}");
+            #endregion
+
+            #region LowercaseXY_Generate
+            BigInteger x = 0;
+            BigInteger y = 0;
+            sw.Start();
+            var ix = GenerateXY(10, 10000); //male x
+            sw.Stop();
+            Console.WriteLine($"Value of x: {ix} | Generation time [ms]: {sw.Elapsed}");
+            sw.Start();
+            var iy = GenerateXY(10, 10000); //male y
+            sw.Stop();
+            Console.WriteLine($"Value of y: {iy} | Generation time [ms]: {sw.Elapsed}");
+            #endregion
+
+            #region CapitalXY_Generate
+            sw.Start();
+            x = GenKey(g, ix, n); //duze X
+            sw.Stop();
+            Console.WriteLine($"Private Key: {x}, generating [ms]: {sw.Elapsed}");
+            sw.Start();
+            y = GenKey(g, iy, n); //duze Y
+            sw.Stop();
+            Console.WriteLine($"Public Key: {y}, generating [ms]: {sw.Elapsed}");
+            #endregion
+
+            var kA = GenKey(y, ix, n);
+            var kB = GenKey(x, iy, n);
+            Console.WriteLine($"kA: {kA}\nkB: {kB}");
         }
+        #endregion
     }
 }
